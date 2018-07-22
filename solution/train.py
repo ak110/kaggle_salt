f6bdd9fd9c62d2fe3709e2c7e170cdb5c590b80b
aki <mark@aur.ll.to>
2018-07-22 20:51:22 +0900

    .
    [INFO ] Accuracy: 0.945
    [INFO ] F1-score: 0.887
    [INFO ] AUC:      0.968
    [INFO ] Logloss:  0.166
    [INFO ] max score: 0.778
    [INFO ] threshold: 0.513

--- solution/train.py
+++ solution/train.py
@@ -57,6 +57,7 @@ def _run(X_train, y_train, X_val, y_val):
         d = builder.conv2d(filters, 1, use_act=False)(d)
         x = keras.layers.add([x, d])
         x = builder.bn_act()(x)
+        x = keras.layers.Dropout(0.25)(x)
         x = builder.conv2d(filters)(x)
         x = builder.conv2d(filters)(x)
 
