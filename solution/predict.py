56aa003ad3ed7220b6da7458877fd1aec440a5e9
aki <mark@aur.ll.to>
2018-07-30 19:48:53 +0900

    vgg

--- solution/predict.py
+++ solution/predict.py
@@ -4,13 +4,13 @@ import pathlib
 import numpy as np
 
 import data
-import model_large
+import model_vgg as last_model
 import pytoolkit as tk
 
 MODELS_DIR = pathlib.Path('models')
 
 ENSEMBLE = False
-THRESHOLD = 0.311
+THRESHOLD = 0.268
 
 
 def _main():
@@ -22,8 +22,8 @@ def _main():
 
 @tk.log.trace()
 def _run():
-    pred_list = model_large.predict(ensemble=ENSEMBLE)
-    pred = np.sum([p > THRESHOLD for p in pred_list]) > len(pred_list) / 2  # hard voting
+    pred_list = last_model.predict(ensemble=ENSEMBLE)
+    pred = np.sum([p > THRESHOLD for p in pred_list], axis=0) > len(pred_list) / 2  # hard voting
     data.save_submission(MODELS_DIR / 'submission.csv', pred)
 
 
