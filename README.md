92260ea04a3f6f338ce97717cc23d5e20806f17e
aki <mark@aur.ll.to>
2018-08-15 23:16:04 +0900

    .

--- README.md
+++ README.md
@@ -6,12 +6,13 @@
     unzip input/test.zip -d input/test
     unzip input/train.zip -d input/train
 
-## TODO
+## Setup
 
 - pip install pydensecrf
 
 ## TODO
 
 - TTA (LR?)
-- cross-pseudo-labeling
+- cross-pseudo-labeling: XyをNoneにしたデータを一定数入れといてgenで処理。cv-index +1のモデルでやると、2,3週はいけるはず。
 - ensemble
+    - predict.pyが最初にキャッシュを削除してからキャッシュディレクトリありで呼び出す感じがよい
--- solution/logcat.sh
+++ solution/logcat.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+echo -e '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n'
 for d in models/* ; do
     if [ -d $d ] ; then
         echo "====================== $d ======================"
         grep 'max score:' $d/train.*.log || grep 'Accuracy:' $d/train.*.log
+        if [ -f reports/$(basename $d).txt ] ; then grep 'max score:' reports/$(basename $d).txt ; fi
     fi
 done
 
