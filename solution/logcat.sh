ba59cb2423497f48ee2589019407e87a1c298ea8
aki <mark@aur.ll.to>
2018-08-18 15:14:34 +0900

    .

--- solution/logcat.sh
+++ solution/logcat.sh
@@ -3,8 +3,8 @@ echo -e '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n'
 for d in models/* ; do
     if [ -d $d ] ; then
         echo "====================== $d ======================"
-        grep 'max score:' $d/train.*.log || grep 'Accuracy:' $d/train.*.log
-        if [ -f reports/$(basename $d).txt ] ; then grep 'max score:' reports/$(basename $d).txt ; fi
+        grep 'max score:' $d/train.*.log || grep --with-filename 'Accuracy:' $d/train.*.log
+        if [ -f reports/$(basename $d).txt ] ; then grep --with-filename 'max score:' reports/$(basename $d).txt ; fi
     fi
 done
 
