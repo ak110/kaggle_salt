99242d03f4e5504babd9ff0575107ab2a1d24336
aki <mark@aur.ll.to>
2018-09-23 05:05:59 +0900

    reg_nas

--- solution/lib/generator.py
+++ solution/lib/generator.py
@@ -2,11 +2,14 @@
 import pytoolkit as tk
 
 
-def create_generator(mode):
+def create_generator(mode, multiple_output=False):
     """generatorの作成。"""
     assert mode in ('bin', 'ss')
+    kwargs = {}
+    if multiple_output:
+        kwargs['output_index'] = 1
 
-    gen = tk.generator.Generator(multiple_input=True)
+    gen = tk.generator.Generator(multiple_input=True, multiple_output=multiple_output)
 
     if mode == 'bin':
         # binでcropは答えが変わっちゃう可能性があるのでやめとく
@@ -14,10 +17,10 @@ def create_generator(mode):
         gen.add(tk.image.RandomPadding(probability=0.25, mode='reflect'), input_index=0)
         gen.add(tk.image.RandomRotate(probability=0.25), input_index=0)
     else:
-        gen.add(tk.image.RandomFlipLR(probability=0.5, with_output=True), input_index=0)
-        gen.add(tk.image.Padding(probability=1, with_output=True), input_index=0)
-        gen.add(tk.image.RandomRotate(probability=0.25, with_output=True), input_index=0)
-        gen.add(tk.image.RandomCrop(probability=1, with_output=True), input_index=0)
+        gen.add(tk.image.RandomFlipLR(probability=0.5, with_output=True), input_index=0, **kwargs)
+        gen.add(tk.image.Padding(probability=1, with_output=True), input_index=0, **kwargs)
+        gen.add(tk.image.RandomRotate(probability=0.25, with_output=True), input_index=0, **kwargs)
+        gen.add(tk.image.RandomCrop(probability=1, with_output=True), input_index=0, **kwargs)
 
     gen.add(tk.image.RandomAugmentors([
         tk.image.RandomBlur(probability=0.125),
@@ -26,9 +29,9 @@ def create_generator(mode):
         tk.image.RandomContrast(probability=0.25),
     ], probability=0.125), input_index=0)
 
-    gen.add(tk.image.Resize((101, 101)), input_index=0)
-
-    if mode == 'ss':
-        gen.add(tk.generator.ProcessOutput(lambda y: tk.ndimage.resize(y, 101, 101)))
+    if mode == 'bin':
+        gen.add(tk.image.Resize((101, 101)), input_index=0)
+    else:
+        gen.add(tk.image.Resize((101, 101), with_output=True), input_index=0, **kwargs)
 
     return gen
