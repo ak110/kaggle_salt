ec2bffd5594be226ceea62c8294eb93905f76221
aki <mark@aur.ll.to>
2018-09-23 14:52:45 +0900

    .

--- solution/logcat.sh
+++ solution/logcat.sh
@@ -3,13 +3,14 @@ echo -e '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n'
 for d in models/* ; do
     if [ -d $d ] ; then
         echo "====================== $d ======================"
-        grep --with-filename ' score:' $d/train.*.log || \
-            grep --with-filename 'Accuracy:' $d/train.*.log || \
-            grep --with-filename 'R^2:' $d/train.*.log
         if [ -f reports/$(basename $d).txt ] ; then
             grep --with-filename ' score:' reports/$(basename $d).txt || \
                 grep --with-filename 'Accuracy:' reports/$(basename $d).txt || \
                 grep --with-filename 'R^2:' reports/$(basename $d).txt
+        else
+            grep --with-filename ' score:' $d/train.*.log || \
+                grep --with-filename 'Accuracy:' $d/train.*.log || \
+                grep --with-filename 'R^2:' $d/train.*.log
         fi
     fi
 done
