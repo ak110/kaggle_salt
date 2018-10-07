684d0b8144354f7af91a885062f64688d29cae1d
aki <mark@aur.ll.to>
2018-10-07 20:39:15 +0900

    .

--- solution/rerun-validate-all1.sh
+++ solution/rerun-validate-all1.sh
@@ -2,3 +2,4 @@
 set -eux
 python3 darknet53_coord_hcs.py validate
 python3 darknet53_mixup.py validate
+python3 darknet53_sepscse.py validate
--- solution/rerun-validate-all3.sh
+++ solution/rerun-validate-all3.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -eux
-python3 darknet53_sepscse.py validate
 python3 bin_nas.py validate
 python3 reg_nas.py validate
