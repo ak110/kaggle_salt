ec3ab83691fc4ae9e7b898a00743315a4b52b328
aki <mark@aur.ll.to>
2018-10-19 04:07:02 +0900

    .

--- solution/logcat.sh
+++ solution/logcat.sh
@@ -3,10 +3,10 @@ echo -e '\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n'
 for d in models/* ; do
     if [ -d $d ] ; then
         echo "====================== $(basename $d).py ======================"
-        if [ -f models/$(basename $d)/validate.txt -a "$1" != "--all" ] ; then
-            grep --with-filename ' score:' models/$(basename $d)/validate.txt || \
-                grep --with-filename 'Accuracy:' models/$(basename $d)/validate.txt || \
-                grep --with-filename 'R^2:' models/$(basename $d)/validate.txt
+        if [ -f models/$(basename $d)/validate.log -a "$1" != "--all" ] ; then
+            grep --with-filename ' score:' models/$(basename $d)/validate.log || \
+                grep --with-filename 'Accuracy:' models/$(basename $d)/validate.log || \
+                grep --with-filename 'R^2:' models/$(basename $d)/validate.log
         else
             grep --with-filename ' score:' $d/train.*.log || \
                 grep --with-filename 'Accuracy:' $d/train.*.log || \
